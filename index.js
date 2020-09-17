774c1d1ecc76005ff9242ee277f57e9b6c98e865
Jorell Langmar <jorell.langmar@gmail.com>
2020-09-17 16:15:42 +0100

    Iteration 6 done and changed the order of each iteration

--- index.js
+++ index.js
@@ -1,63 +1,69 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
+
+mongoose.set("useFindAndModify", false);
 
 // Connection to the database "recipe-app"
 mongoose
-  .connect(MONGODB_URI, {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(self => {
-    console.log(`Connected to the database: "${self.connection.name}"`);
-  //   // Before adding any documents to the database, let's delete all previous entries
-  //   return self.connection.dropDatabase();
-  })
-  // .then(() => {
-  //   Recipe.create({
-  //     "title": "Autre",
-  //     "level": "Amateur Chef",
-  //     "ingredients": [
-  //       "1/2 cup rice vinegar",
-  //       "5 tablespoons honey",
-  //       "1/3 cup soy sauce (such as Silver Swan®)",
-  //       "1/4 cup Asian (toasted) sesame oil",
-  //       "3 tablespoons Asian chili garlic sauce",
-  //       "3 tablespoons minced garlic",
-  //       "salt to taste",
-  //       "8 skinless, boneless chicken thighs"
-  //     ],
-  //     "cuisine": "Asian",
-  //     "dishType": "main_course",
-  //     "image": "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
-  //     "duration": 40,
-  //     "creator": "Chef LePapu"
-  //   })
-  // })
-  // .then (() => {
-  //   Recipe.create(data)
-  //   .then (function recipe(recipe) {for (let i=0 ; i<recipe.length ; i++) {
-  //     console.log(recipe[i].title);
-  //   }})
-  // })
-  // .then(() => {
-  //   Recipe.findOneAndUpdate (
-  //     {title: "Rigatoni alla Genovese"},
-  //     {duration:100},
-  //     {new : true}
-  //   ) .then (console.log("toto"))
-  // })
-  .then(() => {
-    Recipe.deleteOne (
-      {title: "Carrot Cake"},
-    ) .then (console.log("Carrot Cake is no longer available"))
-  })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+	.connect(MONGODB_URI, {
+		useCreateIndex: true,
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then((self) => {
+		console.log(`Connected to the database: "${self.connection.name}"`);
+		//   // Before adding any documents to the database, let's delete all previous entries
+		return self.connection.dropDatabase().then(() => {
+			Recipe.create({
+				title: "Autre",
+				level: "Amateur Chef",
+				ingredients: [
+					"1/2 cup rice vinegar",
+					"5 tablespoons honey",
+					"1/3 cup soy sauce (such as Silver Swan®)",
+					"1/4 cup Asian (toasted) sesame oil",
+					"3 tablespoons Asian chili garlic sauce",
+					"3 tablespoons minced garlic",
+					"salt to taste",
+					"8 skinless, boneless chicken thighs",
+				],
+				cuisine: "Asian",
+				dishType: "main_course",
+				image:
+					"https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+				duration: 40,
+				creator: "Chef LePapu",
+			}).then(() => {
+				Recipe.create(data)
+					.then(function recipe(recipe) {
+						for (let i = 0; i < recipe.length; i++) {
+							console.log(recipe[i].title);
+						}
+					})
+					.then(() => {
+						Recipe.findOneAndUpdate(
+							{ title: "Rigatoni alla Genovese" },
+							{ duration: 100 },
+							{ new: true }
+						)
+							.then(console.log("toto"))
+							.then(() => {
+								Recipe.deleteOne({ title: "Carrot Cake" })
+									.then(console.log("Carrot Cake is no longer available"))
+									.then(() => {
+										mongoose.connection.close();
+									});
+							});
+					});
+			});
+		});
+	})
+	.catch((error) => {
+		console.error("Error connecting to the database", error);
+	});
